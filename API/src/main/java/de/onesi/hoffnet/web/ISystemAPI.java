b42e45f7d4be4de55894895653c76a65256a4735
dusthoff <dustinh@onesi.de>
2018-01-07 08:21:05 +0100

    some documentation

--- API/src/main/java/de/onesi/hoffnet/web/ISystemAPI.java
+++ API/src/main/java/de/onesi/hoffnet/web/ISystemAPI.java
@@ -7,12 +7,14 @@ public interface ISystemAPI {
     /**
      * ContextPath /state
      *
-     * @return JSON String represents current OvenState
+     * @return JSON String represents current de.onesi.hoffnet.states.OvenState
      */
     String state();
 
     /**
      * ContextPath /configure
+     * in State OvenState.READY it can get configured
+     * the workflow starts, when it got successfully configured
      *
      * @param configuration
      * @return
@@ -21,7 +23,7 @@ public interface ISystemAPI {
 
     /**
      * ContextPath /temperature
-     *
+     * at any time you can get the Temperature of the system
      * @return JSON String represents current Temperature Pojo de.onesi.hoffnet.web.data.Temperature
      */
     String temperature();
