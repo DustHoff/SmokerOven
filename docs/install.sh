f7172f97b90841255ee80be881b172e2b9293771
dusthoff <dustinh@onesi.de>
2018-03-24 15:20:36 +0100

    Documentation started

--- docs/install.sh
+++ docs/install.sh
@@ -10,4 +10,12 @@ touch ${BASE}/config/application.yml
 
 ln -s ${BASE}/bin/smokeroven /etc/init.d/smokeroven
 update-rc.d smokeroven defaults
+
+apt-get install libusb-1.0-0 libudev0 pm-utils
+wget -o ${BASE}/brickd.deb http://download.tinkerforge.com/tools/brickd/linux/brickd_linux_latest_armhf.deb
+dpkg -i ${BASE}/brickd.deb
+
+DL_URL=$(curl -s https://api.github.com/repos/DustHoff/SmokerOven/releases/latest | grep browser_download_url | cut -d '"' -f 4)
+wget -o ${BASE}/SmokerOvenApplication.jar "$DL_URL"
+
 ${BASE}/bin/smokeroven start
--- docs/scripts/smokeroven.sh
+++ docs/scripts/smokeroven.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/env bash
-#
-#
-#
-#
-#
-#
+### BEGIN INIT INFO
+# Provides:          smokeroven
+# Required-Start:    $remote_fs $syslog $network
+# Required-Stop:     $remote_fs $syslog $network
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: smokeroven
+# Description
 export HOME=/opt/smokeroven
 export PID=${HOME}/bin/.pid
 
 case $1 in
     start)
-        start-stop-deamon -b -exec /bin/java -- -Xmx1G -Xms1G -Dspring.configuration.location=${HOME}/config -jar ${HOME}/SmokerOvenApplication.jar
+        start-stop-deamon -b --start --startas /bin/java --name smokeroven --pidfile ${PID} -exec /bin/java -- -Xmx1G -Xms1G -Dspring.configuration.location=${HOME}/config -jar ${HOME}/SmokerOvenApplication.jar
     ;;
     stop)
-        start-stop-deamon --stop
-    ;;
-    status)
-        start-stop-deamon --test
+        start-stop-deamon --stop --name smokeroven --pidfile ${PID} -exec /bin/java --retry=0/30/KILL/5
     ;;
     restart)
         $0 stop
         $0 start
     ;;
     *)
-
+        echo "$0 [start|stop|restart]"
 esac
